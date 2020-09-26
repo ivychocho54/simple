9e499bc5f2f38d85ccde4fa55a896f0996df866a
ivychocho54 <68980492+ivychocho54@users.noreply.github.com>
2020-09-26 15:22:29 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+	  private int j;
+        System.out.println("Hello World!");
+
+		System.out.println("Hello Ivy!");
+
+    }
+
+}
